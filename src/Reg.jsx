dcac14b9b3743da65f268a4ed6607fc6678fd063
Vidhya <23eg110a44@anurag.edu.in>
2026-03-10 15:31:25 +0530

    Update Reg.jsx

--- src/Reg.jsx
+++ src/Reg.jsx
@@ -19,7 +19,7 @@ function Reg()
       {
         //  const res=await axios.post("http://localhost:8080/reg",data)
         const res = await axios.post(
-        "https://cabsystemsms-1.onrender.com/register",
+        "https://cabsystemsms-backend-7qbk.onrender.com/register",
         data
       )
          alert(res.data)
